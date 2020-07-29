9883f5ce5b1d55a34bd1f307be5c9d4ec8d1ea0e
Maxim V <maximverbeeck@hotmail.com>
2020-07-29 12:18:30 +0200

    comm vb 1

---
+++ src/oefeningen_vb/arrays/Main.java
@@ -0,0 +1,8 @@
+package oefeningen_vb.arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I love you");
+    }
+}
---
+++ src/oefeningen_vb/interfaces/Main.java
@@ -0,0 +1,8 @@
+package oefeningen_vb.interfaces;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
