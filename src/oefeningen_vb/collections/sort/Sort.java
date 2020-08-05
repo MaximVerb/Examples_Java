7b84d3bb2b13cc1277e513e1edcf489e54c29fc5
Maxim V <maximverbeeck@hotmail.com>
2020-08-05 16:29:24 +0200

    vb

---
+++ src/oefeningen_vb/collections/sort/Sort.java
@@ -0,0 +1,18 @@
+package oefeningen_vb.collections.sort;
+
+import java.util.*;
+
+public class Sort {
+    public static void main(String[] args) {
+        String [] suits = {"Hearts","Diamonds","Clubs","Spades"};
+
+        //create a list containing the array elements
+        List <String> list = Arrays.asList(suits);
+        System.out.printf("Unsorted array elements : %n%s", list);
+
+        System.out.println();
+
+        Collections.sort(list);
+        System.out.printf("Sorted array elements: %n%s", list);
+    }
+}
